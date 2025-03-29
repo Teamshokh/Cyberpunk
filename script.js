b1837369ec2e3ce310447bbe1ced02fbfc842138
Шохжахон Шарипов <teamshokh@gmail.com>
2025-03-29 17:24:01 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Navigate to different pages
+function navigateTo(url) {
+    window.location.href = url;
+}
+
+// Go back to main menu
+function goToMainMenu() {
+    window.location.href = 'index.html';
+}
